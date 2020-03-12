f6b1fba581b2e77588ef2b2b561ca5c2b14bde13
Julien <zejames@gmail.com>
2020-03-12 23:58:29 +0100

    Appli alpha fonctionnel
    Cleanage du code
    Utilisation de @property
    Paramétrage à travers l'interface.
    Intégration Qlocale
    Footer
    R0
    REste un bug lié à l'affichage du board après reset, sur Mac OS uniquement.

--- UX.py
+++ UX.py
@@ -8,10 +8,10 @@ from PyQt5.QtCore import *
 from diseaseBoard import diseaseBoard
 
 DEFAULT_BOARD_SIZE = 50
-DEFAULT_TOUR = 50
+DEFAULT_TOUR = 5
 STATE = {
-    "SAIN":0,
-    "MALADE":1,
+    "SAIN": 0,
+    "MALADE": 1,
     "IMMUN": 2,
     "QUARANTAINE": 3,
     "DECEDE": 4
@@ -19,6 +19,15 @@ STATE = {
 
 STATUS_PLAYING = 1
 STATUS_STOPPED = 0
+
+CONF_LIGNE_TAUX_IMMUNITE = 0
+CONF_LIGNE_PROBA_TRANSMISSION = 1
+CONF_LIGNE_TAUX_MORTALITE = 2
+CONF_LIGNE_DELAI_MORTALITE = 3
+CONF_LIGNE_DELAI_RETABLISSEMENT = 4
+CONF_LIGNE_TAUX_QUARANTAINE = 5
+CONF_LIGNE_DELAI_QUARANTAINE = 6
+
 
 class Pos(QWidget):
     expandable = pyqtSignal(int, int)
@@ -63,37 +72,35 @@ class Pos(QWidget):
         p.setPen(pen)
         p.drawRect(r)
 
-    def reveal(self):
-        self.is_revealed = True
-        self.update()
-
     def click(self):
         pass
         return
-        if not self.is_revealed:
-            self.reveal()
-            if self.adjacent_n == 0:
-                self.expandable.emit(self.x, self.y)
 
-        self.clicked.emit()
+        # self.clicked.emit()
 
     def mouseReleaseEvent(self, e):
         pass
 
+
 class MainWindow(QMainWindow):
-    def __init__(self, board_size, nb_tours, dB, *args, **kwargs):
+    def __init__(self, newboard_size, nb_tours, dB, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.qLocale = QLocale()
 
         self.status = STATUS_STOPPED
 
-        self.board_size = board_size
-
+        self.board_size = newboard_size
         self.diseaseBoard = dB
         self.nb_tours_init = nb_tours
         self.nb_tours = nb_tours
 
+        # w est le Widge QT affiché dans la fenêtre
         w = QWidget()
+
+        # hb : la bande horizontale supérieure de la fenêtre
         hb = QHBoxLayout()
+        hb.addStretch(1)
 
         self.nb_toursLabel = QLabel()
         self.nb_toursLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
@@ -101,23 +108,20 @@ class MainWindow(QMainWindow):
 
         self._timer = QTimer()
         self._timer.timeout.connect(self.update_timer)
-        self._timer.start(100) # in ms
+        self._timer.start(100)  # in ms
 
-        self.goButton = QPushButton()
-        self.goButton.setFixedSize(QSize(52, 32))
-        self.goButton.setText("GO")
+        self.goButton = QPushButton("GO")
+        self.goButton.setFixedSize(QSize(72, 32))
         self.goButton.setFlat(False)
         self.goButton.pressed.connect(self.goButton_pressed)
 
-        self.nextButton = QPushButton()
+        self.nextButton = QPushButton("NEXT")
         self.nextButton.setFixedSize(QSize(72, 32))
-        self.nextButton.setText("NEXT")
         self.nextButton.setFlat(False)
         self.nextButton.pressed.connect(self.nextButton_pressed)
 
-        self.resetButton = QPushButton()
-        self.resetButton.setFixedSize(QSize(112, 32))
-        self.resetButton.setText("RESET")
+        self.resetButton = QPushButton("RESET")
+        self.resetButton.setFixedSize(QSize(82, 32))
         self.resetButton.setFlat(False)
         self.resetButton.pressed.connect(self.resetButton_pressed)
 
@@ -126,44 +130,157 @@ class MainWindow(QMainWindow):
         hb.addWidget(self.nextButton)
         hb.addWidget(self.resetButton)
 
+        # vb permet de mettre en place 2 lignes
+        # La première ligne contient le hb ci-dessous (le header)
+        # La seconde ligne contient un vboxlayout pour la configuration et un gridLayout pour le board
         vb = QVBoxLayout()
         vb.addLayout(hb)
+        vb.addStretch(1)
+
+        # Construction de la partie principale de la fenêtre
+        # A l'aide d'un Hbox, mise en place d'une barre à gauche pour la configuration (QGridLayout de QLabel et de
+        # QLineEdit) puis à droite le board
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addStretch(1)
+
+        self.confgrid = self.setupConfGrid()
 
         self.grid = QGridLayout()
-        self.grid.setSpacing(2)
+        self.grid.setSpacing(1)
+        gridLayout = QVBoxLayout()
+        gridLayout.addStretch(1)
+        gridLayout.addLayout(self.grid)
 
-        vb.addLayout(self.grid)
+        mainLayout.addLayout(self.confgrid)
+        mainLayout.addLayout(gridLayout)
+        vb.addLayout(mainLayout)
+
+        footerLayout = QHBoxLayout()
+        r0Label = QLabel("R0 = " + self.qLocale.toString(self.diseaseBoard.R0))
+        r0Label.setStyleSheet("color: grey")
+        self.r0Label = r0Label
+        footerLayout.addWidget(r0Label)
+        vb.addLayout(footerLayout)
+
         w.setLayout(vb)
         self.setCentralWidget(w)
 
         self.init_map(dB.dernier_etat())
         self.show()
 
+    def setupConfGrid(self):
+
+        confgrid = QGridLayout()
+        confgrid.setSpacing(1)
+
+        TI = QLabel("Taux Immunité : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.tauxImmunite, precision = 2))
+        TIV.setValidator(QDoubleValidator(0.0, 1.0, 2))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_TAUX_IMMUNITE, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_TAUX_IMMUNITE, 1)
+
+        TI = QLabel("Proba transmission : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.probaTransmission, precision = 2))
+        TIV.setValidator(QDoubleValidator(0.0, 1.0, 2))
+        TIV.textChanged.connect(self.updateR0)
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_PROBA_TRANSMISSION, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_PROBA_TRANSMISSION, 1)
+
+        TI = QLabel("Taux Mortalité : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.tauxMortalite, precision = 2))
+        TIV.setValidator(QDoubleValidator(0.0, 1.0, 2))
+        TIV.setValidator(QDoubleValidator(0.0, 1.0, 2))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_TAUX_MORTALITE, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_TAUX_MORTALITE, 1)
+
+        TI = QLabel("Délai Transmission : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.delaiContagion))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.textChanged.connect(self.updateR0)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_DELAI_RETABLISSEMENT, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_DELAI_RETABLISSEMENT, 1)
+
+        TI = QLabel("Délai Mortalité : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.delaiMortalite))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_DELAI_MORTALITE, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_DELAI_MORTALITE, 1)
+
+        TI = QLabel("Taux Quarantaine : ")
+        TIV = QLineEdit(self.qLocale.toString(self.diseaseBoard.tauxQuarantaine, precision = 2))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_TAUX_QUARANTAINE, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_TAUX_QUARANTAINE, 1)
+
+        TI = QLabel("Délai Quarantaine : ")
+        TIV = QLineEdit(str(self.diseaseBoard.delaiQuarantaine))
+        TIV.setAlignment(Qt.AlignRight)
+        TIV.setFixedWidth(40)
+        confgrid.addWidget(TI, CONF_LIGNE_DELAI_QUARANTAINE, 0)
+        confgrid.addWidget(TIV, CONF_LIGNE_DELAI_QUARANTAINE, 1)
+
+        return confgrid
+
     def init_map(self, etat):
-        print(etat)
         # Add positions to the map
         for x in range(0, self.board_size):
             for y in range(0, self.board_size):
                 w = Pos(x, y)
                 self.grid.addWidget(w, x, y)
-                w.redraw(etat[x,y])
+                w.redraw(etat[x, y])
 
     def update_map(self, etat):
         # Add positions to the map
         for x in range(0, self.board_size):
             for y in range(0, self.board_size):
                 w = self.grid.itemAtPosition(x, y).widget()
-                w.redraw(etat[x,y])
+                w.redraw(etat[x, y])
 
     def goButton_pressed(self):
+        if (self.nb_tours_init == self.nb_tours):
+            # Reconfiguration de la simulation
+            self.diseaseBoard.probaTransmission = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_PROBA_TRANSMISSION, 1).widget().text())[0]
+            self.diseaseBoard.tauxMortalite = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_TAUX_MORTALITE, 1).widget().text())[0]
+            self.diseaseBoard.delaiContagion = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_DELAI_RETABLISSEMENT, 1).widget().text())[0]
+            self.diseaseBoard.delaiMortalite = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_DELAI_MORTALITE, 1).widget().text())[0]
+            self.diseaseBoard.tauxQuarantaine = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_TAUX_QUARANTAINE, 1).widget().text())[0]
+            self.diseaseBoard.delaiQuarantaine = self.qLocale.toDouble(
+                self.confgrid.itemAtPosition(CONF_LIGNE_DELAI_QUARANTAINE, 1).widget().text())[0]
+
         if self.status == STATUS_STOPPED:
             self.status = STATUS_PLAYING
+            self.goButton.setText("PAUSE")
+
+            for i in range(self.confgrid.rowCount()):
+                self.confgrid.itemAtPosition(i, 1).widget().setReadOnly(True)
+                self.confgrid.itemAtPosition(i, 1).widget().setStyleSheet("color: grey; selection-color: grey")
+                self.confgrid.itemAtPosition(i, 1).widget().repaint()
+
         else:
             self.status = STATUS_STOPPED
+            self.goButton.setText("GO")
+            for i in range(self.confgrid.rowCount()):
+                self.confgrid.itemAtPosition(i,1).widget().setReadOnly(False)
+                self.confgrid.itemAtPosition(i, 1).widget().setStyleSheet("color: black; selection-color: black")
+                self.confgrid.itemAtPosition(i, 1).widget().repaint()
 
     def nextButton_pressed(self):
         self.nb_tours = self.nb_tours - 1
-        print("next %d" % self.nb_tours)
 
         if self.nb_tours < 0:
             self.status = STATUS_STOPPED
@@ -171,20 +288,20 @@ class MainWindow(QMainWindow):
 
         self.nb_toursLabel.setText("%03d" % self.nb_tours)
         etat = self.diseaseBoard.prochain_tour()
-        print("next : gonna update")
         self.update_map(etat)
-        print("next : updated")
         self.grid.update()
 
     def resetButton_pressed(self):
-        print("Reset pressed")
+        self.diseaseBoard.tauxImmunite = self.qLocale.toDouble(
+            self.confgrid.itemAtPosition(CONF_LIGNE_TAUX_IMMUNITE, 1).widget().text())[0]
+
         self.diseaseBoard.reset(self.nb_tours_init)
-        print("db Reset done")
         self.nb_tours = self.nb_tours_init
         self.nb_toursLabel.setText("%03d" % self.nb_tours)
-
         self.update_map(self.diseaseBoard.dernier_etat())
-        print("init map done")
+
+        self.goButton.setText("GO")
+        self.status = STATUS_STOPPED
 
     def update_timer(self):
         if self.status == STATUS_PLAYING:
@@ -192,29 +309,40 @@ class MainWindow(QMainWindow):
 
             if self.nb_tours < 0:
                 self.status = STATUS_STOPPED
+                for i in range(self.confgrid.rowCount()):
+                    self.confgrid.itemAtPosition(i,1).widget().setReadOnly(False)
+                    self.confgrid.itemAtPosition(i, 1).widget().setStyleSheet("color: black; selection-color: black")
+                    self.confgrid.itemAtPosition(i, 1).widget().repaint()
+
                 return
 
             self.nb_toursLabel.setText("%03d" % self.nb_tours)
             etat = self.diseaseBoard.prochain_tour()
             self.update_map(etat)
 
+    def updateR0(self):
+        self.diseaseBoard.probaTransmission = self.qLocale.toDouble(
+            self.confgrid.itemAtPosition(CONF_LIGNE_PROBA_TRANSMISSION, 1).widget().text())[0]
+        self.diseaseBoard.delaiContagion = self.qLocale.toDouble(
+            self.confgrid.itemAtPosition(CONF_LIGNE_DELAI_RETABLISSEMENT, 1).widget().text())[0]
+        self.r0Label.setText("R0 = {:.3} ".format(self.diseaseBoard.R0))
+
+
 if __name__ == '__main__':
-    tours = DEFAULT_TOUR
-    board_size = 10
+    tours = 60
+    board_size = 30
     nb_clusters = 3
 
     db = diseaseBoard(board_size, tours, nb_clusters)
-    db.setTauxImmunite(0.4)
+    db.tauxImmunite = 0.4
     # Entre 2 et 3 personnes contaminées par malade, si on considère qu'à chaque tour (a peu près un jour), on
     # a l'occasion de contaminer environ 15 personnes, et ce pendant la durée de la contamination, considérée comme
     # égale au délai de rétablissement
 
-    delaiRetablissement = 10
-
-    db.setDelaiRetablissement(delaiRetablissement)
-    db.setProbaContag(2/7/delaiRetablissement)
-    db.setDelaiQuarantaine(6)
-    db.setTauxQuarantaine(0.8)
+    db.delaiContagion = 7
+    db.probaTransmission = 2 / 7
+    db.delaiQuarantaine = 6
+    db.tauxQuarantaine = 0.8
 
     app = QApplication([])
     window = MainWindow(board_size, tours, db)
--- diseaseBoard.py
+++ diseaseBoard.py
@@ -6,8 +6,8 @@ from random import random, randrange
 
 # Couleur associée à l'état
 STATE = {
-    "SAIN":0,
-    "MALADE":1,
+    "SAIN": 0,
+    "MALADE": 1,
     "IMMUN": 2,
     "QUARANTAINE": 3,
     "DECEDE": 4
@@ -29,35 +29,86 @@ class diseaseBoard:
         self._taux_mortalite = 0.03
         self._delai_mortalite = 6
 
-        self._delai_retablissement = 3
+        self._delai_contagion = 3
 
         self._delai_quarantaine = 2
         self._taux_quarantaine  = 0.2
 
         # Par défaut, R0 = 3, et il y a 8 cases adjacentes
-        self._proba_contag = 1 / 8
+        self._proba_transmission = 1 / 8
 
         self._etat_db = []
         self._date_contamination = np.zeros((self._longueur, self._largeur), dtype=int)
 
         self.init_board()
 
-    def setTauxImmunite(self, taux_immunite):
+    #########################
+    # Gestion des attributs #
+    #########################
+
+    @property
+    def tauxImmunite(self):
+        return self._taux_immunite
+
+    @tauxImmunite.setter
+    def tauxImmunite(self, taux_immunite):
         self._taux_immunite = taux_immunite
-        # Nécessité de refaire la map
-        self.init_board()
 
-    def setProbaContag(self, proba_contag):
-        self._proba_contag = proba_contag
+    @property
+    def tauxMortalite(self):
+        return self._taux_mortalite
 
-    def setTauxQuarantaine(self, taux_quarantaine):
+    @tauxMortalite.setter
+    def tauxMortalite(self, taux_mortalite):
+        self._taux_mortalite = taux_mortalite
+
+    @property
+    def delaiMortalite(self):
+        return self._delai_mortalite
+
+    @delaiMortalite.setter
+    def delaiMortalite(self, delai_mortalite):
+        self._delai_mortalite = delai_mortalite
+
+    @property
+    def probaTransmission(self):
+        return self._proba_transmission
+
+    @probaTransmission.setter
+    def probaTransmission(self, proba_transmission):
+        self._proba_transmission = proba_transmission
+
+    @property
+    def tauxQuarantaine(self):
+        return self._taux_quarantaine
+
+    @tauxQuarantaine.setter
+    def tauxQuarantaine(self, taux_quarantaine):
         self._taux_quarantaine = taux_quarantaine
 
-    def setDelaiQuarantaine(self, delai_quarantaine):
+    @property
+    def delaiQuarantaine(self):
+        return self._delai_quarantaine
+
+    @delaiQuarantaine.setter
+    def delaiQuarantaine(self, delai_quarantaine):
         self._delai_quarantaine = delai_quarantaine
 
-    def setDelaiRetablissement(self, delai_retablissement):
-        self._delai_retablissement = delai_retablissement
+    @property
+    def delaiContagion(self):
+        return self._delai_contagion
+
+    @delaiContagion.setter
+    def delaiContagion(self, delai_contagion):
+        self._delai_contagion = delai_contagion
+
+    @property
+    def R0(self):
+        return (self._proba_transmission * self._delai_contagion)
+
+    ###################################
+    # Fin de la gestion des attributs #
+    ###################################
 
     def init_board(self):
         etat0 = np.zeros((self._longueur, self._largeur), dtype=int)
@@ -81,29 +132,32 @@ class diseaseBoard:
         self._etat_db = []
         self._date_contamination = np.zeros((self._longueur, self._largeur), dtype=int)
         self._nb_tour = nb_tours
+        self._tour_actuel = 0
 
         self.init_board()
 
     def prochain_tour(self):
+        neighbours = []
         etat_actuel = self._etat_db[-1]
         etat = etat_actuel.copy()
 
         for x in range(self._longueur):
-            for y in range (self._largeur):
-                if etat_actuel[x,y] == STATE["QUARANTAINE"] or etat_actuel[x,y] == STATE["MALADE"]:
-                    if self._tour_actuel - self._date_contamination[x, y] == self._delai_retablissement:
-                        etat[x,y] = STATE["IMMUN"]
-                        continue # Nécessité de faire continue car le test STATE["MْALADE"] se fait sur etat_actuel
+            for y in range(self._largeur):
+                if etat_actuel[x, y] == STATE["QUARANTAINE"] or etat_actuel[x, y] == STATE["MALADE"]:
+                    if self._tour_actuel - self._date_contamination[x, y] == self._delai_contagion:
+                        etat[x, y] = STATE["IMMUN"]
+                        continue  # Nécessité de faire continue car le test STATE["MْALADE"] se fait sur etat_actuel
 
                     if self._tour_actuel - self._date_contamination[x, y] == self._delai_mortalite:
                         if random() <= self._taux_mortalite:
-                            etat[x,y] = STATE["DECEDE"]
+                            etat[x, y] = STATE["DECEDE"]
                             continue
 
-                if etat_actuel[x,y] == STATE["MALADE"]:
+                if etat_actuel[x, y] == STATE["MALADE"]:
                     if self._tour_actuel - self._date_contamination[x, y] == self._delai_quarantaine:
                         if random() <= self._taux_quarantaine:
-                            etat[x,y] = STATE["QUARANTAINE"]
+                            etat[x, y] = STATE["QUARANTAINE"]
+                            continue
 
                     #
                     # Contamination des voisins
@@ -111,9 +165,9 @@ class diseaseBoard:
 
                     # Case "coin", où l'on a 3 voisins
                     if x == 0 and y == 0:
-                        neighbours = [[0,1], [1,1], [1,0]]
+                        neighbours = [[0, 1], [1, 1], [1, 0]]
                     if x == (self._longueur - 1) and y == 0:
-                        neighbours = [[x - 1 ,0], [x - 1,1], [x,1]]
+                        neighbours = [[x - 1, 0], [x - 1, 1], [x, 1]]
                     if x == 0 and y == (self._largeur - 1):
                         neighbours = [[0, y - 1], [1, y - 1], [1, y]]
                     if x == (self._longueur - 1) and y == (self._largeur - 1):
@@ -130,14 +184,14 @@ class diseaseBoard:
                         neighbours = [[x - 1, y], [x + 1, y], [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]]
 
                     # Autres cas : 8 cases
-                    if x > 0 and x < (self._longueur - 1) and y > 0 and y < (self._largeur - 1):
+                    if 0 < x < (self._longueur - 1) and 0 < y < (self._largeur - 1):
                         neighbours = [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1],
-                                      [x, y -1], [x, y + 1],
+                                      [x, y - 1], [x, y + 1],
                                       [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]]
 
                     for nb in neighbours:
                         if etat_actuel[nb[0], nb[1]] == STATE["SAIN"]:
-                            if random() < self._proba_contag:
+                            if random() < self._proba_transmission:
                                 etat[nb[0], nb[1]] = STATE["MALADE"]
                                 self._date_contamination[nb[0], nb[1]] = self._tour_actuel
 
@@ -157,154 +211,7 @@ class diseaseBoard:
         nb_decede = 0
         for x in range(self._longueur):
             for y in range(self._largeur):
-                if self._etat_db[-1][x,y] == STATE["DECEDE"]:
+                if self._etat_db[-1][x, y] == STATE["DECEDE"]:
                     nb_decede = nb_decede + 1
 
         return nb_decede
-
-# if __name__ == '__main__':
-#     db = diseaseBoard(20, 15, 0.2)
-#     db.loop()
-#
-#     print("--\n  Bilan : {:,} sur {:,}" . format(db.nb_mort(), 200 * 200))
-
-# #
-# # Modèle
-# #
-#
-# # Dimension du monde
-# LONGUEUR = 200
-# LARGEUR = 200
-# NOMBRE_TOUR = 50
-#
-#
-#
-# #
-# # Paramètres
-# #
-#
-# # Circulation de l'épidémie
-#
-# DENSITE_IMMUNITE     = 0.2
-#
-# # Taux de transmission du virus (R0)
-#
-# R0                  = 3
-# PROBA_CONTAG         = R0 / 8
-#
-# # Parametre de mortalité
-# TAUX_MORTALITE       = 0.03
-# DELAI_MORTALITE      = 6
-#
-# # Retablissement si pas mortalité
-# DELAI_RETABLISSEMENT = 7
-#
-# # Délai moyen de mise en quarantaine (obligatoirement inférieur au décès)
-# # La quarantaine peut être soi un isolement à domicile soit dans un hopital
-# #
-# DELAI_QUARANTAINE    = 4
-# TAUX_QUARANTAINE     = 0.0
-#
-# def initialiser_monde(p_immu):
-#     etat0 = np.zeros((LONGUEUR, LARGEUR), dtype=int)
-#     etat0[0:LONGUEUR, 0:LARGEUR] = STATE["SAIN"]
-#
-#     # Creation de la population immunisée
-#     for x in range(LONGUEUR):
-#         for y in range(LARGEUR):
-#             if random() < p_immu:
-#                 etat0[x,y] = STATE["IMMUN"]
-#
-#     return etat0
-#
-# def prochain_tour(etat, p_conta, num_tour):
-#     nv_etat = etat.copy()
-#
-#     for x in range(LONGUEUR):
-#         for y in range (LARGEUR):
-#             if nv_etat[x,y] == STATE["QUARANTAINE"]:
-#                 if num_tour - dateContamination[x, y] == DELAI_RETABLISSEMENT:
-#                     nv_etat[x,y] = STATE["IMMUN"]
-#
-#             if nv_etat[x,y] == STATE["MALADE"]:
-#                 #
-#                 # Décès ou rétablissement
-#                 #
-#
-#                 if num_tour - dateContamination[x, y] == DELAI_MORTALITE:
-#                     if random() <= TAUX_MORTALITE:
-#                         nv_etat[x,y] = STATE["DECEDE"]
-#                         continue
-#
-#                 if num_tour - dateContamination[x, y] == DELAI_RETABLISSEMENT:
-#                     nv_etat[x,y] = STATE["IMMUN"]
-#
-#                 if num_tour - dateContamination[x, y] == DELAI_QUARANTAINE:
-#                     if random() <= TAUX_QUARANTAINE:
-#                         nv_etat[x,y] = STATE["QUARANTAINE"]
-#
-#                 #
-#                 # Contamination des voisins
-#                 #
-#
-#                 # Case "coin", où l'on a 3 voisins
-#                 if x == 0 and y == 0:
-#                     neighbours = [[0,1], [1,1], [1,0]]
-#                 if x == (LONGUEUR - 1) and y == 0:
-#                     neighbours = [[x - 1 ,0], [x - 1,1], [x,1]]
-#                 if x == 0 and y == (LARGEUR - 1):
-#                     neighbours = [[0, y - 1], [1, y - 1], [1, y]]
-#                 if x == (LONGUEUR - 1) and y == (LARGEUR - 1):
-#                     neighbours = [[x - 1, y], [x - 1, y - 1], [x, y - 1]]
-#
-#                 # Case "bord" mais pas coin, où l'on 5 voisins
-#                 if x == 0 and not (y == 0 or y == (LARGEUR - 1)):
-#                     neighbours = [[0, y - 1], [0, y + 1], [1, y - 1], [1, y], [1, y + 1]]
-#                 if x == (LONGUEUR - 1) and not (y == 0 or y == (LARGEUR - 1)):
-#                     neighbours = [[x, y - 1], [x, y + 1], [x - 1, y - 1], [x - 1, y], [x - 1, y + 1]]
-#                 if not (x == 0 or x == (LONGUEUR - 1)) and y == 0:
-#                     neighbours = [[x - 1, 0], [x + 1, 0], [x - 1, 1], [x, 1], [x + 1, 1]]
-#                 if not (x == 0 or x == (LONGUEUR - 1)) and y == (LARGEUR - 1):
-#                     neighbours = [[x - 1, y], [x + 1, y], [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]]
-#
-#                 # Autres cas : 8 cases
-#                 if x > 0 and x < (LONGUEUR - 1) and y > 0 and y < (LARGEUR - 1):
-#                     neighbours = [[x - 1, y - 1], [x - 1, y], [x - 1, y + 1],
-#                                   [x, y -1], [x, y + 1],
-#                                   [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]]
-#
-#                 for nb in neighbours:
-#                     if nv_etat[nb[0], nb[1]] == STATE["SAIN"]:
-#                         if random() < p_conta:
-#                             nv_etat[nb[0], nb[1]] = STATE["MALADE"]
-#                             dateContamination[nb[0], nb[1]] = num_tour
-#
-#     return nv_etat
-#
-# Boucle main
-#
-
-# if __name__ == '__main__':
-#     etat_db = []
-#     dateContamination = np.zeros((LONGUEUR, LARGEUR), dtype=int)
-#     etat0 = initialiser_monde(DENSITE_IMMUNITE)
-#
-#     x0 = randrange(LARGEUR)
-#     y0 = randrange(LONGUEUR)
-#     etat0[x0, y0] = STATE["MALADE"]
-#     dateContamination[x0, y0] = -1
-#
-#     etat_db.append(etat0)
-#
-#     for i in range(NOMBRE_TOUR):
-#         nv_etat = prochain_tour(etat_db[-1], PROBA_CONTAG, i)
-#         etat_db.append(nv_etat)
-#     #    print (etat)
-#
-#     NbDecede = 0
-#     for x in range(LONGUEUR):
-#         for y in range(LARGEUR):
-#             if etat_db[-1][x,y] == STATE["DECEDE"]:
-#                 NbDecede = NbDecede + 1
-#
-#     print("--\n  Bilan : {:,} sur {:,}" . format(NbDecede, LONGUEUR * LARGEUR))
